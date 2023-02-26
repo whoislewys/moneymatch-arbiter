4b4fd0b979e4b7bb89110132fe7e10b984640098
whoislewys <whoislewys@gmail.com>
2023-02-26 12:40:07 -0700

    nu types

--- realtimeFileReads.js
+++ realtimeFileReads.js
@@ -1,22 +1,39 @@
 "use strict";
-/// Stream game events, watch for game end event, push signed game results to proper Escrow contract
-// const { SlippiGame } = require('@slippi/slippi-js');
-// const os = require('os');
-// const chokidar = require('chokidar');
-// const _ = require('lodash');
-// const { ethers } = require('ethers');
-// const { EscrowFactory__factory } = require('./types/ethers-contracts/factories/contracts/EscrowFactory__factory.ts');
-// require('dotenv').config();
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
 var _a;
 exports.__esModule = true;
@@ -41,6 +58,172 @@ dotenv.config();
 var provider = new ethers_1.ethers.providers.JsonRpcProvider('http://localhost:8545');
 var arbiterKey = (_a = process.env.ARBITER_KEY) !== null && _a !== void 0 ? _a : ethers_1.ethers.constants.AddressZero;
 var arbiterWallet = new ethers_1.ethers.Wallet(arbiterKey, provider);
+console.log('arbiter address: ', arbiterWallet.address);
+var getCurrentGameEscrow = function (settings) { return __awaiter(void 0, void 0, void 0, function () {
+    var EscrowFactory, player1Id, player2Id, currentGameFilter, events, latestEvent, currentGameEscrowAddress, contractPlayer1Id, contractPlayer1Address, contractPlayer2Id, contractPlayer2Address, EscrowContract;
+    var _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                EscrowFactory = EscrowFactory__factory_1.EscrowFactory__factory.connect((_a = process.env.ESCROW_FACTORY_ADDRESS) !== null && _a !== void 0 ? _a : ethers_1.ethers.constants.AddressZero, arbiterWallet);
+                console.log('Got EscrowFactory contract');
+                player1Id = settings.players[0].connectCode;
+                console.log('player1Id: ', player1Id);
+                player2Id = settings.players[1].connectCode;
+                console.log('player2Id: ', player2Id);
+                currentGameFilter = EscrowFactory.filters.EscrowCreated(player1Id, null, player2Id, null, null);
+                return [4 /*yield*/, EscrowFactory.queryFilter(currentGameFilter)];
+            case 1:
+                events = _b.sent();
+                latestEvent = events[events.length - 1];
+                currentGameEscrowAddress = latestEvent.args[4];
+                console.log('currentGameEscrowAddress: ', currentGameEscrowAddress);
+                contractPlayer1Id = latestEvent.args[0];
+                console.log('contractPlayer1Id: ', contractPlayer1Id);
+                contractPlayer1Address = latestEvent.args[1];
+                console.log('contractPlayer1Address: ', contractPlayer1Address);
+                contractPlayer2Id = latestEvent.args[2];
+                console.log('contractPlayer2Id: ', contractPlayer2Id);
+                contractPlayer2Address = latestEvent.args[3];
+                console.log('contractPlayer2Address: ', contractPlayer2Address);
+                EscrowContract = Escrow__factory_1.Escrow__factory.connect(currentGameEscrowAddress, arbiterWallet);
+                console.log('EscrowContract.address: ', EscrowContract.address);
+                return [2 /*return*/, EscrowContract];
+        }
+    });
+}); };
+var handleChange = function (path) { return __awaiter(void 0, void 0, void 0, function () {
+    var gamewinners, metadata, gameState, settings, gameEnd, currentEscrow, gameByPath, game, endTypes, endMessage, lrasText, winningPlayer, winnerId;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                try {
+                    gameByPath = {};
+                    game = (0, lodash_1.get)(gameByPath, [path, 'game']);
+                    // Create SlippiGame object
+                    if (!game) {
+                        console.log("New game file at: ".concat(path));
+                        // Make sure to enable `processOnTheFly` to get updated stats as the game progresses
+                        game = new slippi_js_1.SlippiGame(path, { processOnTheFly: true });
+                        gameByPath[path] = {
+                            game: game,
+                            state: {
+                                settings: null,
+                                detectedPunishes: {}
+                            }
+                        };
+                    }
+                    gameState = (0, lodash_1.get)(gameByPath, [path, 'state']);
+                    settings = game.getSettings();
+                    metadata = game.getMetadata();
+                    gameEnd = game.getGameEnd();
+                    gamewinners = game.getWinners();
+                }
+                catch (err) {
+                    console.log(err);
+                    return [2 /*return*/];
+                }
+                if (!(!gameState.settings && settings)) return [3 /*break*/, 2];
+                console.log("[Game Start] New game has started");
+                console.log('settings: ', settings);
+                gameState.settings = settings;
+                return [4 /*yield*/, getCurrentGameEscrow(settings)];
+            case 1:
+                currentEscrow = _a.sent();
+                _a.label = 2;
+            case 2:
+                // dev
+                if (true) {
+                    gameEnd = {
+                        gameEndMethod: 2,
+                        lrasInitiatorIndex: -1,
+                        placements: [
+                            { playerIndex: 0, position: 0 },
+                            { playerIndex: 1, position: 1 },
+                            { playerIndex: 2, position: -1 },
+                            { playerIndex: 3, position: -1 },
+                        ]
+                    };
+                    endTypes = {
+                        1: 'TIME!',
+                        2: 'GAME!',
+                        7: 'No Contest'
+                    };
+                    console.log('\n');
+                    endMessage = (0, lodash_1.get)(endTypes, gameEnd.gameEndMethod) || 'Unknown';
+                    lrasText = gameEnd.gameEndMethod === 7
+                        ? " | Quitter Index: ".concat(gameEnd.lrasInitiatorIndex)
+                        : '';
+                    console.log("[Game Complete] Type: ".concat(endMessage).concat(lrasText));
+                }
+                // dev
+                gamewinners = [
+                    {
+                        playerIndex: 0,
+                        names: {
+                            code: 'TARC#448'
+                        }
+                    },
+                ];
+                if (!(gamewinners.length !== 0 && metadata)) return [3 /*break*/, 4];
+                winningPlayer = metadata.players[gamewinners[0].playerIndex];
+                winnerId = winningPlayer.names.code;
+                console.log('winnerId: ', winnerId);
+                // Sign game results in EIP-712 format
+                // const domain = {
+                //   // Must match the signing domain in the contract
+                //   name: 'MoneyMatch',
+                //   version: '1',
+                //   verifyingContract: currentEscrow.address,
+                //   chainId: 1337, // local
+                //   // chainId: 437, // polygon
+                //   // chainId: 80001 // polygon mumbai
+                // };
+                // const types = {
+                //   GameResults: [{ name: 'winnerId', type: 'string' }],
+                // };
+                // const winner = {
+                //   // winnerAddress: winnerId === player1Id ? player1Address : player2Address,
+                //   winnerId,
+                // };
+                // const signature = await arbiterWallet._signTypedData(domain, types, winner) 
+                // const gameResults = {
+                //   ...winner,
+                //   signature,
+                // };
+                // await currentEscrow.endGame(gameResults);
+                return [4 /*yield*/, currentEscrow.endGame(winnerId)];
+            case 3:
+                // Sign game results in EIP-712 format
+                // const domain = {
+                //   // Must match the signing domain in the contract
+                //   name: 'MoneyMatch',
+                //   version: '1',
+                //   verifyingContract: currentEscrow.address,
+                //   chainId: 1337, // local
+                //   // chainId: 437, // polygon
+                //   // chainId: 80001 // polygon mumbai
+                // };
+                // const types = {
+                //   GameResults: [{ name: 'winnerId', type: 'string' }],
+                // };
+                // const winner = {
+                //   // winnerAddress: winnerId === player1Id ? player1Address : player2Address,
+                //   winnerId,
+                // };
+                // const signature = await arbiterWallet._signTypedData(domain, types, winner) 
+                // const gameResults = {
+                //   ...winner,
+                //   signature,
+                // };
+                // await currentEscrow.endGame(gameResults);
+                _a.sent();
+                _a.label = 4;
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+// Watch for new game
 var listenPath = "".concat(os.homedir(), "/Slippi/");
 console.log("Listening for slippi games at: ".concat(listenPath));
 var watcher = chokidar.watch(listenPath, {
@@ -50,110 +233,10 @@ var watcher = chokidar.watch(listenPath, {
     usePolling: true,
     ignoreInitial: true
 });
-var gameByPath = {};
-watcher.on('change', function (path) {
-    var _a;
-    // TODO: see when game starts to call startGame() function on the Escrow conch;
-    var start = Date.now();
-    console.log('CHANGE EVENT');
-    var gamewinners;
-    var metadata;
-    var gameState;
-    var settings;
-    var frames;
-    var latestFrame;
-    var gameEnd;
-    try {
-        var game = lodash_1["default"].get(gameByPath, [path, 'game']);
-        if (!game) {
-            // console.log(`New file at: ${path}`);
-            // Make sure to enable `processOnTheFly` to get updated stats as the game progresses
-            game = new slippi_js_1.SlippiGame(path, { processOnTheFly: true });
-            gameByPath[path] = {
-                game: game,
-                state: {
-                    settings: null,
-                    detectedPunishes: {}
-                }
-            };
-        }
-        gameState = lodash_1["default"].get(gameByPath, [path, 'state']);
-        settings = game.getSettings();
-        metadata = game.getMetadata();
-        frames = game.getFrames();
-        latestFrame = game.getLatestFrame();
-        gameEnd = game.getGameEnd();
-        gamewinners = game.getWinners();
-    }
-    catch (err) {
-        console.log(err);
-        return;
-    }
-    if (!gameState.settings && settings) {
-        console.log("[Game Start] New game has started");
-        console.log(settings);
-        gameState.settings = settings;
-    }
-    console.log("We have ".concat(lodash_1["default"].size(frames), " frames."));
-    lodash_1["default"].forEach(settings.players, function (player) {
-        var frameData = lodash_1["default"].get(latestFrame, ['players', player.playerIndex]);
-        if (!frameData) {
-            return;
-        }
-    });
-    if (gameEnd) {
-        // NOTE: These values and the quitter index will not work until 2.0.0 recording code is
-        // NOTE: used. This code has not been publicly released yet as it still has issues
-        var endTypes = {
-            1: 'TIME!',
-            2: 'GAME!',
-            7: 'No Contest'
-        };
-        console.log('\n');
-        var endMessage = lodash_1["default"].get(endTypes, gameEnd.gameEndMethod) || 'Unknown';
-        var lrasText = gameEnd.gameEndMethod === 7
-            ? " | Quitter Index: ".concat(gameEnd.lrasInitiatorIndex)
-            : '';
-        console.log("[Game Complete] Type: ".concat(endMessage).concat(lrasText));
-    }
-    if (gamewinners.length !== 0 && metadata) {
-        var winningPlayer = metadata.players[gamewinners[0].playerIndex];
-        var winnerId_1 = winningPlayer.names.code; // ex. JAWA#977
-        // Get the current game's Escrow address by using ids of players to search through EscrowFactory event logs
-        var EscrowFactory = EscrowFactory__factory_1.EscrowFactory__factory.connect((_a = process.env.ESCROW_FACTORY_ADDRESS) !== null && _a !== void 0 ? _a : ethers_1.ethers.constants.AddressZero, arbiterWallet);
-        var player1Id = metadata.players[0].names.code;
-        var player2Id = metadata.players[1].names.code;
-        var currentGameFilter = EscrowFactory.filters.EscrowCreated(player1Id, null, player2Id, null, null);
-        var currentGameLogs = EscrowFactory.queryFilter(currentGameFilter).then(function (events) {
-            var latestEvent = events[events.length - 1];
-            var currentGameEscrowAddress = latestEvent.args[4];
-            var player1Id = latestEvent.args[0];
-            var player1Address = latestEvent.args[1];
-            var player2Id = latestEvent.args[2];
-            var player2Address = latestEvent.args[3];
-            // Get escrow contract for this game
-            var EscrowContract = Escrow__factory_1.Escrow__factory.connect(currentGameEscrowAddress, arbiterWallet);
-            // Sign game results in EIP-712 format
-            var domain = {
-                // Must match the signing domain in the contract
-                name: 'MoneyMatch',
-                version: '1',
-                verifyingContract: currentGameEscrowAddress,
-                chainId: 1337
-            };
-            var types = {
-                GameResults: [{ name: 'winnerId', type: 'string' }]
-            };
-            var winner = {
-                // winnerAddress: winnerId === player1Id ? player1Address : player2Address,
-                winnerId: winnerId_1
-            };
-            arbiterWallet
-                ._signTypedData(domain, types, winner)
-                .then(function (signature) {
-                var gameResults = __assign(__assign({}, winner), { signature: signature });
-                EscrowContract.endGame(gameResults);
-            });
-        });
-    }
-});
+// dev
+handleChange('/Users/machine/Slippi/Game_20230213T193024.slp');
+// enddev
+// watcher.on('change', (path) => {
+//   // prod
+//   handleChange(path);
+// });
--- realtimeFileReads.ts
+++ realtimeFileReads.ts
@@ -1,21 +1,12 @@
-/// Stream game events, watch for game end event, push signed game results to proper Escrow contract
-// const { SlippiGame } = require('@slippi/slippi-js');
-// const os = require('os');
-// const chokidar = require('chokidar');
-// const _ = require('lodash');
-// const { ethers } = require('ethers');
-// const { EscrowFactory__factory } = require('./types/ethers-contracts/factories/contracts/EscrowFactory__factory.ts');
-// require('dotenv').config();
-
 import { SlippiGame } from '@slippi/slippi-js';
 import * as os from 'os';
 import * as chokidar from 'chokidar';
-import _ from 'lodash';
+import { size, get, forEach } from 'lodash';
 import { ethers } from 'ethers';
 import { EscrowFactory__factory } from './types/ethers-contracts/factories/contracts/EscrowFactory__factory';
 import { Escrow__factory } from './types/ethers-contracts/factories/contracts/Escrow__factory';
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 // MUMBAI
 // const provider = new ethers.providers.JsonRpcProvider(
@@ -29,36 +20,63 @@ dotenv.config()
 const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
 const arbiterKey = process.env.ARBITER_KEY ?? ethers.constants.AddressZero;
 const arbiterWallet = new ethers.Wallet(arbiterKey, provider);
+console.log('arbiter address: ', arbiterWallet.address);
 
-const listenPath = `${os.homedir()}/Slippi/`;
-console.log(`Listening for slippi games at: ${listenPath}`);
-const watcher = chokidar.watch(listenPath, {
-  ignored: '!*.slp', // TODO: This doesn't work. Use regex?
-  depth: 0,
-  persistent: true,
-  usePolling: true,
-  ignoreInitial: true,
-});
+const getCurrentGameEscrow = async (settings: any) => {
+  // Get the current game's Escrow address by using ids of players to search through EscrowFactory event logs
+  const EscrowFactory = EscrowFactory__factory.connect(
+    process.env.ESCROW_FACTORY_ADDRESS ?? ethers.constants.AddressZero,
+    arbiterWallet
+  );
+  console.log('Got EscrowFactory contract');
+  const player1Id = settings.players[0].connectCode;
+  console.log('player1Id: ', player1Id);
+  const player2Id = settings.players[1].connectCode;
+  console.log('player2Id: ', player2Id);
+  const currentGameFilter = EscrowFactory.filters.EscrowCreated(
+    player1Id,
+    null,
+    player2Id,
+    null,
+    null
+  );
 
-const gameByPath: any = {};
+  const events = await EscrowFactory.queryFilter(currentGameFilter);
+  const latestEvent = events[events.length - 1];
+  const currentGameEscrowAddress = latestEvent.args[4];
+  console.log('currentGameEscrowAddress: ', currentGameEscrowAddress);
+  const contractPlayer1Id = latestEvent.args[0];
+  console.log('contractPlayer1Id: ', contractPlayer1Id);
+  const contractPlayer1Address = latestEvent.args[1];
+  console.log('contractPlayer1Address: ', contractPlayer1Address);
+  const contractPlayer2Id = latestEvent.args[2];
+  console.log('contractPlayer2Id: ', contractPlayer2Id);
+  const contractPlayer2Address = latestEvent.args[3];
+  console.log('contractPlayer2Address: ', contractPlayer2Address);
 
-watcher.on('change', (path) => {
-  // TODO: see when game starts to call startGame() function on the Escrow conch;
-  const start = Date.now();
-  console.log('CHANGE EVENT');
+  // Get escrow contract for this game
+  const EscrowContract = Escrow__factory.connect(
+    currentGameEscrowAddress,
+    arbiterWallet
+  );
+  console.log('EscrowContract.address: ', EscrowContract.address);
+  return EscrowContract;
+};
 
+const handleChange = async (path: any) => {
   let gamewinners: any;
   let metadata: any;
   let gameState: any;
   let settings: any;
-  let frames: any;
-  let latestFrame: any;
   let gameEnd: any;
+  let currentEscrow: any;
 
   try {
-    let game = _.get(gameByPath, [path, 'game']);
+    const gameByPath: any = {};
+    let game = get(gameByPath, [path, 'game']);
+    // Create SlippiGame object
     if (!game) {
-      // console.log(`New file at: ${path}`);
+      console.log(`New game file at: ${path}`);
       // Make sure to enable `processOnTheFly` to get updated stats as the game progresses
       game = new SlippiGame(path, { processOnTheFly: true });
       gameByPath[path] = {
@@ -70,12 +88,10 @@ watcher.on('change', (path) => {
       };
     }
 
-    gameState = _.get(gameByPath, [path, 'state']);
+    gameState = get(gameByPath, [path, 'state']);
 
     settings = game.getSettings();
     metadata = game.getMetadata();
-    frames = game.getFrames();
-    latestFrame = game.getLatestFrame();
     gameEnd = game.getGameEnd();
     gamewinners = game.getWinners();
   } catch (err) {
@@ -83,23 +99,31 @@ watcher.on('change', (path) => {
     return;
   }
 
+  // When game has started, get the Escrow contract and call startGame
   if (!gameState.settings && settings) {
     console.log(`[Game Start] New game has started`);
-    console.log(settings);
+    console.log('settings: ', settings);
     gameState.settings = settings;
+    currentEscrow = await getCurrentGameEscrow(settings);
+    // await currentEscrow.startGame();
   }
 
-  console.log(`We have ${_.size(frames)} frames.`);
-  _.forEach(settings.players, (player: any) => {
-    const frameData = _.get(latestFrame, ['players', player.playerIndex]);
-    if (!frameData) {
-      return;
-    }
-  });
+  // dev
+  if (true) {
+    gameEnd = {
+      gameEndMethod: 2,
+      lrasInitiatorIndex: -1,
+      placements: [
+        { playerIndex: 0, position: 0 },
+        { playerIndex: 1, position: 1 },
+        { playerIndex: 2, position: -1 },
+        { playerIndex: 3, position: -1 },
+      ],
+    };
+  // enddev
 
-  if (gameEnd) {
-    // NOTE: These values and the quitter index will not work until 2.0.0 recording code is
-    // NOTE: used. This code has not been publicly released yet as it still has issues
+    // prod
+    // if (gameEnd) {
     const endTypes = {
       1: 'TIME!',
       2: 'GAME!',
@@ -108,7 +132,7 @@ watcher.on('change', (path) => {
 
     console.log('\n');
 
-    const endMessage = _.get(endTypes, gameEnd.gameEndMethod) || 'Unknown';
+    const endMessage = get(endTypes, gameEnd.gameEndMethod) || 'Unknown';
 
     const lrasText =
       gameEnd.gameEndMethod === 7
@@ -117,67 +141,66 @@ watcher.on('change', (path) => {
     console.log(`[Game Complete] Type: ${endMessage}${lrasText}`);
   }
 
+  // dev
+  gamewinners = [
+    {
+      playerIndex: 0,
+      names: {
+        code: 'TARC#448',
+      },
+    },
+  ];
+  // enddev
+
   if (gamewinners.length !== 0 && metadata) {
     const winningPlayer = metadata.players[gamewinners[0].playerIndex];
-    const winnerId = winningPlayer.names.code; // ex. JAWA#977
+    const winnerId = winningPlayer.names.code; // ex. TARC#448
+    console.log('winnerId: ', winnerId);
 
-    // Get the current game's Escrow address by using ids of players to search through EscrowFactory event logs
-    const EscrowFactory = EscrowFactory__factory.connect(
-      process.env.ESCROW_FACTORY_ADDRESS ?? ethers.constants.AddressZero,
-      arbiterWallet
-    );
-    const player1Id = metadata.players[0].names.code;
-    const player2Id = metadata.players[1].names.code;
-    const currentGameFilter = EscrowFactory.filters.EscrowCreated(
-      player1Id,
-      null,
-      player2Id,
-      null,
-      null
-    );
-    const currentGameLogs = EscrowFactory.queryFilter(currentGameFilter).then(
-      (events) => {
-        const latestEvent = events[events.length - 1];
-        const currentGameEscrowAddress = latestEvent.args[4];
-        const player1Id = latestEvent.args[0];
-        const player1Address = latestEvent.args[1];
-        const player2Id = latestEvent.args[2];
-        const player2Address = latestEvent.args[3];
+    // Sign game results in EIP-712 format
+    // const domain = {
+    //   // Must match the signing domain in the contract
+    //   name: 'MoneyMatch',
+    //   version: '1',
+    //   verifyingContract: currentEscrow.address,
+    //   chainId: 1337, // local
+    //   // chainId: 437, // polygon
+    //   // chainId: 80001 // polygon mumbai
+    // };
+    // const types = {
+    //   GameResults: [{ name: 'winnerId', type: 'string' }],
+    // };
+    // const winner = {
+    //   // winnerAddress: winnerId === player1Id ? player1Address : player2Address,
+    //   winnerId,
+    // };
+    // const signature = await arbiterWallet._signTypedData(domain, types, winner) 
+    // const gameResults = {
+    //   ...winner,
+    //   signature,
+    // };
+    // await currentEscrow.endGame(gameResults);
 
-        // Get escrow contract for this game
-        const EscrowContract = Escrow__factory.connect(
-          currentGameEscrowAddress,
-          arbiterWallet
-        );
-
-        // Sign game results in EIP-712 format
-        const domain = {
-          // Must match the signing domain in the contract
-          name: 'MoneyMatch',
-          version: '1',
-          verifyingContract: currentGameEscrowAddress,
-          chainId: 1337, // local
-          // chainId: 437, // polygon
-          // chainId: 80001 // polygon mumbai
-        };
-        const types = {
-          GameResults: [{ name: 'winnerId', type: 'string' }],
-        };
-        const winner = {
-          // winnerAddress: winnerId === player1Id ? player1Address : player2Address,
-          winnerId,
-        };
-
-        arbiterWallet
-          ._signTypedData(domain, types, winner)
-          .then((signature) => {
-            const gameResults = {
-              ...winner,
-              signature,
-            };
-            EscrowContract.endGame(gameResults);
-          });
-      }
-    );
+    await currentEscrow.endGame(winnerId);
   }
+};
+
+// Watch for new game
+const listenPath = `${os.homedir()}/Slippi/`;
+console.log(`Listening for slippi games at: ${listenPath}`);
+const watcher = chokidar.watch(listenPath, {
+  ignored: '!*.slp', // TODO: This doesn't work. Use regex?
+  depth: 0,
+  persistent: true,
+  usePolling: true,
+  ignoreInitial: true,
 });
+
+// dev
+handleChange('/Users/machine/Slippi/Game_20230213T193024.slp');
+// enddev
+
+// watcher.on('change', (path) => {
+//   // prod
+//   handleChange(path);
+// });
